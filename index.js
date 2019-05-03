1815073be52b208815c936208d4a06e28ec3e7be
Simon Jones <sjones3@VONFVFXR0RQHV2G.local>
2019-05-03 15:00:38 +0200

    Addex basic use of Nexmo library

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 //Import the necessary libraries/declare the necessary objects
 // express is used to create a simple web server
 var express = require("express");
+// 
+const Nexmo = require('nexmo');
 // body-parser is a third-party parsing library.
 var myParser = require("body-parser");
 var app = express();
+var PORT = 8080;
 
 //use the body parser middleware in express
 app.use(myParser.urlencoded({extended : false}));
@@ -18,9 +21,45 @@ app.param('appId', function(request, response, next, appId) {
 
 
 // Create a route for /numbers
-  app.post("/numbers/", function(request, response) {
-     console.log(request.body); //This prints the JSON document received (if it is a JSON document)
-     response.sendStatus(200);
+// An example inbound GET would be {domain}/numbers?api_key=xxxxf&api_secret=zzzz&type=landline&country=DE&features=SMS,VOICE
+
+  app.get("/numbers", function(request, response) {
+  
+  const nexmo = new Nexmo({
+    apiKey: request.query.api_key,
+    apiSecret: request.query.api_secret,
+//    applicationId: request.query.api_key,
+  }, );
+   
+//  var result = nexmo.number.get;
+//console.log(nexmo.number.get);
+//var json = nexmo.number.get;
+//nexmo.number.get(response.status(200).json(this));
+
+nexmo.number.get(function(err,rows){
+            if(err) {
+                response.status(500).json({"Error" : true, "Message" : "Error executing request"});
+            } else {
+                response.status(200).json(rows);
+}
+        });
+
+
+
+//nexmo.number.get(rows);
+//response.json ({"Error" : false, "Message" : "Success", "Users" : rows});
+
+  
+//  console.log("params", Object.assign(request.query, request.body)) 
+//  response.status(204).send()
+//}
+
+//console.log (json);
+  //  response.setHeader('Content-Type', 'application/json')
+//    response.json(json);
+
+    console.log(request.query.api_key,request.query.api_secret); //This prints the JSON document received (if it is a JSON document)
+//    response.sendStatus(200);
 
 });
 
@@ -41,4 +80,4 @@ app.param('appId', function(request, response, next, appId) {
 
 
 //Start the server and make it listen for connections on port 8080 
-app.listen(8080);
+app.listen(PORT);
