21a41444a2a7b4bcba991f1b152ddef02e98c337
Simon Jones <sjones3@VONFVFXR0RQHV2G.local>
2019-05-05 12:07:53 +0200

    test version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Import the necessary libraries/declare the necessary objects
 // express is used to create a simple web server
 var express = require("express");
-// 
+// \
 const Nexmo = require('nexmo');
 // body-parser is a third-party parsing library.
 var myParser = require("body-parser");
@@ -40,6 +40,7 @@ nexmo.number.get(function(err,rows){
 // An example inbound GET would be {domain}/application/?app_id=xxxxxx-xxxx-xxxx-xxxx-xxxxxx&api_key=xxxxxxx&api_secret=xxxxxx
   app.get("/application", function(request, response) {
 
+          
 // Get URL parameters from request  
   const nexmo = new Nexmo({
     apiKey: request.query.api_key,
@@ -56,7 +57,7 @@ nexmo.applications.get(request.query.app_id,function(err,rows){
         });
 
      console.log('Processed request for  details of this application ' + request.query.app_id);
-
+          
 });
 
   app.get("/applications", function(request, response) {
@@ -84,7 +85,7 @@ nexmo.application.get(function(err,rows){
 // An example inbound POST would be {domain}/application/?app_id=1a648832-af31-4c90-9330-5e11689e63f4&api_key=xxxxxx&api_secret=yyyyyy&country=de&msisdn=498944314615001&action=configure
 
 app.post("/application", function(request, response) {
-        
+         console.log("Running POST /application");
         // Get URL parameters from request
         const nexmo = new Nexmo({
                                 apiKey: request.query.api_key,
@@ -105,7 +106,7 @@ app.post("/application", function(request, response) {
                                response.status(200).json(rows);
                                }
                                });
-         
+         };
          // Send request to Nexmo using Nexmo node libraries and respond with json values
          if (request.query.action == "unconfigure") {
          nexmo.number.update(request.query.country, request.query.msisdn, function(err,rows){
@@ -116,20 +117,8 @@ app.post("/application", function(request, response) {
                              }
                              });
          
+         };
          
-         
-         
-         
-         
-         then}
-         
-         nexmo.application.get(function(err,rows){
-                               if(err) {
-                               response.status(500).json({"Error" : true, "Message" : "Error executing request"});
-                               } else {
-                               response.status(200).json(rows);
-                               }
-                               });
          
          console.log('Processed request for list of all applications associated with account ' + request.query.api_key);
          
